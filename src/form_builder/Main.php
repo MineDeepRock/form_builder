e47646b9bf9b2a95842f8387d4c1e72f53d0deeb
suinua <maatinosippo@gmail.com>
2020-06-16 17:16:25 +0900

    fix

--- src/form_builder/Main.php
+++ src/form_builder/Main.php
@@ -11,8 +11,4 @@ class Main extends PluginBase implements Listener
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-
-    public function onJoin(PlayerJoinEvent $event) {
-        $event->getPlayer()->sendForm(new CustomFomSample($this->getServer(), $event->getPlayer()));
-    }
 }
